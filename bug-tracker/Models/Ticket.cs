731b5ab2e79be8910c5c21dde0542594a3821072
Jeremy Persing <jpersing1999@gmail.com>
2021-01-30 07:24:14 -0800

    UI changes

--- bug-tracker/Models/Ticket.cs
+++ bug-tracker/Models/Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace bug_tracker.Models
@@ -8,9 +9,23 @@ namespace bug_tracker.Models
     public class Ticket
     {
         public int Id { get; set; }
+
+        [Display(Name = "Ticket Name")]
+        [Required(ErrorMessage = "Please enter a name for your ticket")]
         public string TicketName { get; set; }
+
+        [Display(Name = "Ticket Description")]
+        [Required(ErrorMessage = "Please enter a description for your ticket")]
         public string TicketDescription { get; set; }
+
+        [Display(Name = "Ticket Status")]
+        [Required(ErrorMessage = "Please select a status for your ticket")]
         public string TicketStatus { get; set; }
+
+        [Display(Name = "Start Name")]
+        [Required(ErrorMessage = "Please select a start date for your ticket")]
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
 
         public Ticket()
         {
