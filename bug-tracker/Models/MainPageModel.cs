02c75a631aa85779d0cc97013455842e37966c0e
Jeremy Persing <jpersing1999@gmail.com>
2021-01-31 16:52:55 -0800

    Style changes

---
+++ bug-tracker/Models/MainPageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bug_tracker.Models
+{
+    public class MainPageModel
+    {
+        public Project Project { get; set; }
+        public Ticket Ticket { get; set; }
+    }
+}
