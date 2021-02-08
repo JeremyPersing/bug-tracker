bdfb89ce81cb65b7ef61e9145e8b69adbca6a32c
Jeremy Persing <jpersing1999@gmail.com>
2021-02-07 17:43:22 -0800

    CSS additions

---
+++ bug-tracker/Models/ProjectsWithTicketsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bug_tracker.Models
+{
+    public class ProjectsWithTicketsModel
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+
+        public List<TicketModel> Tickets { get; set; }
+
+    }
+}
