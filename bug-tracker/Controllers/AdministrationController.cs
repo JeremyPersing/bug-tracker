a8359783c2e18361130266a4aac6fde5a4c9d9e6
Jeremy Persing <jpersing1999@gmail.com>
2021-02-09 18:39:30 -0800

    Beginning of user roles

---
+++ bug-tracker/Controllers/AdministrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace bug_tracker.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}
---
+++ bug-tracker/Models/CreateRoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bug_tracker.Models
+{
+    public class CreateRoleModel
+    {
+        [Required]
+        [Display(Name = "Roll")];
+        public string RoleName { get; set; }
+    }
+}
