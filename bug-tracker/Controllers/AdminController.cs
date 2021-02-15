f216182cbe06e75c645e47498e59fd8cada8a59a
Jeremy Persing <jpersing1999@gmail.com>
2021-02-14 21:37:28 -0800

    Username added

---
+++ bug-tracker/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using bug_tracker.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace bug_tracker.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+
+        
+    }
+}
